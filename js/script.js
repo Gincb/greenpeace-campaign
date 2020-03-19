8541b8ff381414fdfe2f916326aad704417af51e
Gintare B <gintare.b@gmail.com>
2020-03-19 13:03:38 +0100

    move map

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+const mapL = document.querySelector(".map-left");
+const mapR = document.querySelector(".map-right");
+const map = document.querySelector(".map");
 
+console.log(mapL)
+
+mapL.addEventListener("click", addMapLeft);
+mapR.addEventListener("click", addMapRight);
+
+function addMapLeft() {
+    if(map.classList.contains("center")) {
+        map.classList.remove("center");
+        map.classList.add("left");
+    } else {
+        map.classList.remove("right");
+        map.classList.add("center");
+    }
+}
+
+function addMapRight() {
+    if(map.classList.contains("center")) {
+        map.classList.remove("center");
+        map.classList.add("right");
+    } else {
+        map.classList.remove("left");
+        map.classList.add("center");
+    }
+}
