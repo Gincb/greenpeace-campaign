12da9845bd3d4b08a69d88821447b34e8187524d
suppmen <suppmen@gmail.com>
2020-03-24 10:17:12 +0100

    some Fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const mapL = document.querySelector(".map-left");
 const mapR = document.querySelector(".map-right");
 const map = document.querySelector(".map");
 
+
 mapL.addEventListener("click", addMapLeft);
 mapR.addEventListener("click", addMapRight);
 
@@ -23,4 +24,17 @@ function addMapRight() {
         map.classList.remove("left");
         map.classList.add("center");
     }
+}
+
+const newTab1 = document.getElementById("Url1");
+newTab1.addEventListener("click", openUrl1);
+function openUrl1(){
+    window.open("https://www.graesrod.org/", "_blank");
+}
+
+
+const newTab2 = document.getElementById("Url2");
+newTab2.addEventListener("click", openUrl2);
+function openUrl2(){
+    window.open("https://www.greenpeace.org/international/", "_blank");
 }
