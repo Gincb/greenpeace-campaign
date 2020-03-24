6aaaf77bc3dc58f1a5746191040eb0120face7af
Gintare B <gintare.b@gmail.com>
2020-03-24 12:17:06 +0100

    Amazon Pin

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+// MAP
+
 const mapL = document.querySelector(".map-left");
 const mapR = document.querySelector(".map-right");
 const map = document.querySelector(".map");
-
+const pinAmazon = document.querySelector(".pin-amazon");
 
 mapL.addEventListener("click", addMapLeft);
 mapR.addEventListener("click", addMapRight);
@@ -10,9 +12,17 @@ function addMapLeft() {
     if(map.classList.contains("center")) {
         map.classList.remove("center");
         map.classList.add("left");
+        mapL.classList.add("dn");
+        mapR.classList.remove("dn");
+        pinAmazon.classList.add("dl");
+        pinAmazon.classList.remove("dn");
     } else {
         map.classList.remove("right");
         map.classList.add("center");
+        mapL.classList.remove("dn");
+        mapR.classList.remove("dn");
+        pinAmazon.classList.remove("dl");
+        pinAmazon.classList.add("dn");
     }
 }
 
@@ -20,9 +30,17 @@ function addMapRight() {
     if(map.classList.contains("center")) {
         map.classList.remove("center");
         map.classList.add("right");
+        mapR.classList.add("dn");
+        mapL.classList.remove("dn");
+        pinAmazon.classList.remove("dl");
+        pinAmazon.classList.add("dn");
     } else {
         map.classList.remove("left");
         map.classList.add("center");
+        mapR.classList.remove("dn");
+        mapL.classList.remove("dn");
+        pinAmazon.classList.remove("dl");
+        pinAmazon.classList.add("dn");
     }
 }
 
@@ -37,4 +55,27 @@ const newTab2 = document.getElementById("Url2");
 newTab2.addEventListener("click", openUrl2);
 function openUrl2(){
     window.open("https://www.greenpeace.org/international/", "_blank");
+}
+
+// PINS
+
+const video = document.getElementById('video-yt');
+const button = document.getElementById('play-video');
+const close = document.getElementById('modalId');
+const myAmazon = document.getElementById('myAmazon');
+
+button.addEventListener("click", addFullscreen);
+myAmazon.addEventListener("click", addClose);
+close.addEventListener("click", addClose);
+
+function addFullscreen(e) {
+    var isPause = video.src.indexOf('autoplay=0') != -1;
+    video.src = isPause ? video.src.replace('autoplay=0', 'autoplay=1') :
+        video.src.replace('autoplay=1', 'autoplay=0');
+}
+
+function addClose(e) {
+    var isPause = video.src.indexOf('autoplay=1') != 1;
+    video.src = isPause ? video.src.replace('autoplay=1', 'autoplay=0') :
+        video.src.replace('autoplay=0', 'autoplay=1');
 }
